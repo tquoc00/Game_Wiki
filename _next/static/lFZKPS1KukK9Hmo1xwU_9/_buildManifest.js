887a3b45401d63fc089ef77276644d3d75a68183
tquoc00 <tquoc00@users.noreply.github.com>
2026-07-16 09:25:11 +0000

    deploy: f481ea5ad509fb6538fd297485cc0623b17d2d76

---
+++ _next/static/lFZKPS1KukK9Hmo1xwU_9/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Game_Wiki//_next/:path+",
+        "destination": "/Game_Wiki/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
